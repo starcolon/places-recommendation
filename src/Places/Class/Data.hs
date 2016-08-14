190f338a0b4635c0061653427a95bc2807fdc614
starcolon <tao@starcolon.com>
2016-08-14 13:28:04 +0700

    (WIP) Attempt to parse CSV into data package

--- src/Places/Class/Data.hs
+++ src/Places/Class/Data.hs
@@ -1,24 +1,53 @@
 module Places.Class.Data (
 
-  Place
+  Place,
+  showPlace,
+  fromList
 
 ) where
 
 -- address,category,id,lat,lng,location,
 -- name,originalId,polarity,subCategory,
 -- details,reviews
-data Place = Place {
-  address     :: String
-, category    :: String
-, id          :: String
-, lat         :: Double
-, lng         :: Double
-, location    :: String
-, name        :: String
-, originalId  :: String
-, polarity    :: String
-, subCategory :: String
-, details     :: String
-, reviews     :: String
-}
+data Place = 
+  Place { address     :: String
+        , category    :: String
+        , _id         :: Integer
+        , lat         :: Double
+        , lng         :: Double
+        , location    :: String
+        , name        :: String
+        , originalId  :: Integer
+        , polarity    :: String
+        , subCategory :: String
+        , details     :: String
+        , reviews     :: String
+        }
+
+instance Show Place where
+  show = showPlace
+
+
+showPlace :: Place -> String
+showPlace p = let { name'     = name p
+                  ; location' = location p
+                  ; cate'     = subCategory p
+                  ; line      = [name', "(", cate', ") in ", location']
+              } in unlines line
+
+fromList :: [String] -> Place
+fromList [a1:a2:a3:a4:a5:a6:a7:a8:a9:a10:a11:a12] = 
+  Place { address     = a1
+        , category    = a2
+        , _id         = read a3 :: Integer
+        , lat         = read a4 :: Double
+        , lng         = read a5 :: Double
+        , location    = a6
+        , name        = a7
+        , originalId  = read a8 :: Integer
+        , polarity    = a9
+        , subCategory = a10
+        , details     = a11
+        , reviews     = a12
+  }
 
--- src/Places/CsvInput/Parser.hs
+++ src/Places/CsvInput/Parser.hs
@@ -2,9 +2,9 @@ module Places.CsvInput.Parser where
 
 import Text.ParserCombinators.Parsec
 import Data.CSV
-import Places.Class.Data
+import Places.Class.Data as PlaceData
 
--- | Read CSV file 
+-- | Read CSV file into lists of values
 readCSV :: String -> IO ([[String]])
 readCSV path = do 
   csv <- parseFromFile csvFile path
@@ -15,5 +15,8 @@ readCSV path = do
       return []
     Right (n:ns) -> return ns
 
--- toPlace :: [String] -> IO (Place)
--- toPlace vs = -- TAOTODO:
+-- | Convert a list of values to a [Place] data
+toPlace :: [String] -> Place
+toPlace ns = PlaceData.fromList ns
+
+  
