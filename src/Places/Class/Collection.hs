ca19c7a18a61ee566019f90706e8a69a5cd551e2
starcolon <tao@starcolon.com>
2016-08-15 16:12:30 +0700

    (WIP) Draft [Collection] class

--- src/Places/Class/Collection.hs
+++ src/Places/Class/Collection.hs
@@ -1,6 +1,20 @@
 module Places.Class.Collection where
 
 import Data.List
-import Data.HashMap.Strict
+import Data.Trees.KdTree
 
-data PlaceMap = PlaceMap {}
+-- | Mapping of places and their geospatial location
+class PlaceMap m where
+
+  -- Filtering the places with a boolean predicate
+  filter :: m -> (p -> Bool) -> m
+
+
+-- | Implementation of Places
+instance PlaceMap [Place] where
+  filter = filterPlaces
+
+
+-- | Filter a list of [Place]
+filterPlaces :: Place p => [p] -> (p -> Bool) -> [p]
+filterPlaces places f = filter f places
