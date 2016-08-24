2df15d9a766d3950ea99e12c44c042c71e638d84
starcolon <tao@starcolon.com>
2016-08-24 20:42:52 +0900

    (WIP) Draft instances of [PlaceList]

--- src/Places/Class/Instance.hs
+++ src/Places/Class/Instance.hs
@@ -1,9 +1,18 @@
 module Places.Class.Instance where
 
-import Data.Monoid
-import Places.Class.Data
+import           Data.Monoid
+import           Places.Class.Data
+import qualified Data.Trees.KdTree as Kd-- hiding (fromList)
 
--- | A filterable Monad of [PlaceList]
---instance Monad PlaceList where
---  return a = 
---  m >>= g  =
+newtype PlaceTree = KdTree Place
+
+instance PlaceList PlaceTree where
+  closest  = findClosest
+  whichAre = filterCate
+
+
+findClosest :: Int -> Place -> PlaceTree
+findClosest = error "TAOTODO: Implement this"
+
+filterCate :: String -> PlaceTree
+filterCate = error "TAOTODO: Implement this"
